308d239d0f38bd33fb5e051a47f2c6c3d1b5a017
YTG2315 <152232174+YTG2315@users.noreply.github.com>
2026-08-14 21:19:43 -0400

    Update myApp.js
    Signed-off-by: YTG2315 <152232174+YTG2315@users.noreply.github.com>

--- myApp.js
+++ myApp.js
@@ -18,63 +18,8 @@ app.use(
     },
     hsts: {
       maxAge: ninetyDaysInSeconds,
-      force: true
+      force: true // Included if following freeCodeCamp's specific test requirements
     },
     noCache: true
-  }}
-         );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+  })
+);
