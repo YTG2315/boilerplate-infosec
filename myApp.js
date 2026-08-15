1c601d5b34d12c82dd092d3c35cc294af332b714
YTG2315 <152232174+YTG2315@users.noreply.github.com>
2026-08-14 21:16:18 -0400

    Update myApp.js
    Signed-off-by: YTG2315 <152232174+YTG2315@users.noreply.github.com>

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
-const bcrypt = require('bcrypt'):
+const bcrypt = require('bcrypt');
 
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
