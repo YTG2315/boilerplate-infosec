2f25a0e0dc142497e9870583327e660a7951b666
YTG2315 <152232174+YTG2315@users.noreply.github.com>
2026-08-14 21:07:02 -0400

    Update myApp.js
    Signed-off-by: YTG2315 <152232174+YTG2315@users.noreply.github.com>

--- myApp.js
+++ myApp.js
@@ -2,25 +2,26 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives:{
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", 'trusted-cdn.com']
-    }
-  })
-  );
-
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
-app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true}));
 
+app.use(
+  helmet({
+    frameguard: {
+      action: 'deny'
+    },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", 'trusted-cdn.com']
+      }
+    },
+    hsts: {
+      maxAge: ninetyDaysInSeconds,
+      force: true
+    },
+    noCache: true
+  }}
+         );
 
 
 
