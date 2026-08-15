d1804778cff6d672755a8d1a0ff16188214a676a
YTG2315 <152232174+YTG2315@users.noreply.github.com>
2026-08-14 20:30:44 -0400

    Update myApp.js
    Signed-off-by: YTG2315 <152232174+YTG2315@users.noreply.github.com>

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helment.xssFilter());
+app.use(helmet.xssFilter());
 
 
 
