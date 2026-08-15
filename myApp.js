59fdcc374817982742181657ad11d7fb3c13001c
YTG2315 <152232174+YTG2315@users.noreply.github.com>
2026-08-14 20:42:54 -0400

    Update myApp.js
    Signed-off-by: YTG2315 <152232174+YTG2315@users.noreply.github.com>

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(helemt.hsts({ maxAge: ninetyDaysInSeconds, force: true}));
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
